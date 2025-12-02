1a2b8623045c4f1eb9e8d2a83b78bbae2ec4cae7
sindhunerlige <sindhunerlige@gmail.com>
2025-12-02 12:53:17 +0530

    adding a java file

---
+++ Employee.java
@@ -0,0 +1,22 @@
+class Employee{
+    public static void main(String[] args){
+        System.out.println("employe name");
+        System.out.println("employe id");
+        System.out.println("employe phone number");
+        System.out.println("gender");
+        System.out.println("date of birth");
+        System.out.println("education");
+        System.out.println("city");
+        System.out.println("account details");
+        System.out.println("status");
+        System.out.println("salary date");
+
+
+
+
+
+
+
+
+    }
+}
