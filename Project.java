63ecf4032b0990be46670fd13bcb8d33b9816abe
sindhunerlige <sindhunerlige@gmail.com>
2025-12-02 12:26:54 +0530

    hi

---
+++ Project.java
@@ -0,0 +1,15 @@
+class Project {
+    public class static void main(String[] args) {
+        System.out.println("Printing multiple lines.");
+        System.out.println("Learning Java basics.");
+        System.out.println("Understanding classes.");
+        System.out.println("Understanding main method.");
+        System.out.println("Debugging common errors.");
+        System.out.println("Practicing print statements.");
+        System.out.println("Java is case sensitive.");
+        System.out.println("File name must match class name.");
+        System.out.println("Program executed successfully.");
+        
+}
+    }
+
