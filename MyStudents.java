e5bce146b6febc3fa4d3d45d94f77cc144ddf9db
sindhunerlige <sindhunerlige@gmail.com>
2025-12-03 10:48:13 +0530

    students files

---
+++ MyStudents.java
@@ -0,0 +1,14 @@
+class MyStudents {
+    public static void main(String[] args) {
+        System.out.println("student id");
+        System.out.println("attendence");
+        System.out.println("student name");
+        System.out.println("student marks");
+        System.out.println("fees payment");
+        System.out.println("result");
+        System.out.println("traport department");
+        System.out.println("phone number");
+        System.out.println("documents");
+        System.out.println("guide name");
+    }
+}
