65672df8830f8d99bc3fe318f6db5b94e1829656
sindhunerlige <sindhunerlige@gmail.com>
2025-12-03 10:44:35 +0530

    bankaccount number

---
+++ Bankaccount.java
@@ -0,0 +1,15 @@
+class Bankaccount {
+    public static void main(String[] args){
+        System.out.println("Bank account");
+        System.out.println("Account holder details verified");
+        System.out.println("Initial deposit");
+        System.out.println("Balance");
+        System.out.println("Withdrawal request");
+        System.out.println("Transaction history");
+        System.out.println("Fund transfer");
+        System.out.println("Account summary");
+        System.out.println("Customer support");
+        System.out.println("Banking system");
+
+    }
+}
