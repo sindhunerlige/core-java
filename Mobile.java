efeb7c8dd932cc1c10831164cbb31f0ebfc099e6
sindhunerlige <sindhunerlige@gmail.com>
2025-12-03 10:46:55 +0530

    mobile brands name

---
+++ Mobile.java
@@ -0,0 +1,7 @@
+class Mobile {
+    public static void main(String[] args){
+        System.out.print("Mobile name");
+        System.out.print("price");
+        System.out.print("camera");
+   }
+}
