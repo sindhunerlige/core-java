a91087e037070a4e02deb520f28d797cb554dcf1
sindhunerlige <sindhunerlige@gmail.com>
2025-12-03 10:34:35 +0530

    Movie file uploading

---
+++ Movie.java
@@ -0,0 +1,8 @@
+class Movie {
+    public static void main(String[] args){
+        System.out.print("Movie name");
+        System.out.print("hero");
+        System.out.print("heroin");
+        System.out.print("language");
+    }
+}
