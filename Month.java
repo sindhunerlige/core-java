5c0deee2147b12d06a27a6a057b2051a3e17f60d
sindhunerlige <sindhunerlige@gmail.com>
2025-12-05 10:57:44 +0530

    month

---
+++ Month.java
@@ -0,0 +1,47 @@
+class Month{
+public static void main(String[] args){
+int value = 5;
+monthInYear(value);
+}
+public static void monthInYear(int value){
+if(value==1){
+System.out.println("january");
+}
+else if(value==2){
+System.out.println("february");
+}
+else if(value==3){
+System.out.println("march");
+}
+else if(value==4){
+System.out.println("april");
+}
+else if(value==5){
+System.out.println("may");
+}
+else if(value==6){
+System.out.println("june");
+}
+else if(value==7){
+System.out.println("july");
+}
+else if(value==8){
+System.out.println("august");
+}
+else if(value==9){
+System.out.println("septumber");
+}
+else if(value==10){
+System.out.println("october");
+}
+else if(value==11){
+System.out.println("november");
+}
+else if(value==12){
+System.out.println("december");
+}
+else{
+	System.out.println("no months in a year");
+}
+}
+}
