d167c797bfe998f72f0a98824db0c56120101cae
sindhunerlige <sindhunerlige@gmail.com>
2025-12-09 22:42:34 +0530

    asciivaluesforcapitalwords

---
+++ AsciiValueCapital.java
@@ -0,0 +1,139 @@
+class AsciiValueCapital{
+public static void main(String[] args){
+char charater = 'B';
+if(charater == 'A'){
+System.out.println("Ascii value of 'A'is "+65);
+}
+else if(charater == 'B'){
+System.out.println("Ascii value of 'B' is "+66);
+}
+else if(charater == 'C'){
+System.out.println("Ascii value of 'C' is "+67);
+}
+else if(charater == 'D'){
+System.out.println("Ascii value of 'D' is "+68);
+}
+else if(charater == 'E'){
+System.out.println("Ascii value of 'E' is "+69);
+}
+else if(charater == 'F'){
+System.out.println("Ascii value of 'F' is "+70);
+}
+else if(charater == 'G'){
+System.out.println("Ascii value of 'G' is "+71);
+}
+else if(charater == 'H'){
+System.out.println("Ascii value of 'H' is "+72);
+}
+else if(charater == 'I'){
+System.out.println("Ascii value of 'I' is "+73);
+}
+else if(charater == 'J'){
+System.out.println("Ascii value of 'J' is "+74);
+}
+else if(charater == 'K'){
+System.out.println("Ascii value of 'K' is "+75);
+}
+else if(charater == 'L'){
+System.out.println("Ascii value of 'L' is "+76);
+}
+else if(charater == 'M'){
+System.out.println("Ascii value of 'M' is "+77);
+}
+else if(charater == 'N'){
+System.out.println("Ascii value of 'N' is "+78);
+}
+else if(charater == 'O'){
+System.out.println("Ascii value of 'O' is "+79);
+}
+else if(charater == 'P'){
+System.out.println("Ascii value of 'P' is "+80);
+}
+else if(charater == 'Q'){
+System.out.println("Ascii value of 'Q' is "+81);
+}
+else if(charater == 'R'){
+System.out.println("Ascii value of 'R' is "+82);
+}
+else if(charater == 'S'){
+System.out.println("Ascii value of 'S' is "+83);
+}
+else if(charater == 'T'){
+System.out.println("Ascii value of 'T' is "+84);
+}
+else if(charater == 'U'){
+System.out.println("Ascii value of 'U' is "+85);
+}
+else if(charater == 'V'){
+System.out.println("Ascii value of 'V' is "+86);
+}
+else if(charater == 'W'){
+System.out.println("Ascii value of 'W' is "+87);
+}
+else if(charater == 'X'){
+System.out.println("Ascii value of 'X' is "+88);
+}
+else if(charater == 'Y'){
+System.out.println("Ascii value of 'Y' is "+89);
+}
+else if(charater == 'Z'){
+System.out.println("Ascii value of 'Z' is "+90);
+}
+else{
+System.out.println("invalid charater");
+}
+switch(charater){
+case 'A' :System.out.println("Ascii value of 'A'is "+65);
+           break;
+case 'B' :System.out.println("Ascii value of 'B'is "+66);
+           break;
+case 'C' :System.out.println("Ascii value of 'C'is "+68);
+           break;
+case 'D' :System.out.println("Ascii value of 'D'is "+69);
+           break;
+case 'E' :System.out.println("Ascii value of 'E'is "+70);
+           break;
+case 'F' :System.out.println("Ascii value of 'F'is "+71);
+           break;		   
+case 'G' :System.out.println("Ascii value of 'G'is "+72);
+           break;
+case 'H' :System.out.println("Ascii value of 'H'is "+73);
+           break;
+case 'I' :System.out.println("Ascii value of 'I'is "+74);
+           break;	
+case 'J' :System.out.println("Ascii value of 'J'is "+75);
+           break;
+case 'L' :System.out.println("Ascii value of 'L'is "+76);
+           break;	
+case 'M' :System.out.println("Ascii value of 'M'is "+77);
+           break;
+case 'N' :System.out.println("Ascii value of 'N'is "+78);
+           break;
+case 'O' :System.out.println("Ascii value of 'O'is "+79);
+           break;
+case 'P' :System.out.println("Ascii value of 'P'is "+80);
+           break;
+case 'Q' :System.out.println("Ascii value of 'Q'is "+81);
+           break;
+case 'R' :System.out.println("Ascii value of 'R'is "+82);
+           break;
+case 'S' :System.out.println("Ascii value of 'S'is "+83);
+           break;		   
+case 'T' :System.out.println("Ascii value of 'T'is "+84);
+           break;
+case 'U' :System.out.println("Ascii value of 'U'is "+85);
+           break;
+case 'V' :System.out.println("Ascii value of 'V'is "+86);
+           break;		   
+case 'W' :System.out.println("Ascii value of 'W'is "+87);
+           break;
+case 'X' :System.out.println("Ascii value of 'X'is "+88);
+           break;		  
+case 'Y' :System.out.println("Ascii value of 'Y'is "+89);
+           break;
+case 'Z' :System.out.println("Ascii value of 'Z'is "+90);
+           break;
+}
+}
+}
+		   
