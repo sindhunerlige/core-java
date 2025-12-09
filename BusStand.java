475a75d4228ee9d6bea55540905641b7fbf8028f
sindhunerlige <sindhunerlige@gmail.com>
2025-12-09 10:47:35 +0530

    busstand

---
+++ BusStand.java
@@ -0,0 +1,80 @@
+public class BusStand {
+    public static void main(String args[]){
+        int i=5;
+        for(int num=1;num<=10;num++){
+            if(i==1&&num==1){
+                System.out.println("Silk Board");
+            }else if(i==2&&num==2){
+                System.out.println("Majestic");
+            }else if(i==3&&num==3){
+                System.out.println("MICO Layout");
+            }else if(i==4&&num==4){
+                System.out.println("BTM Layout");
+            }else if(i==5&&num==5){
+                System.out.println("Kasturi Nagar");
+            }else if(i==6&&num==6){
+                System.out.println("BTM WaterTank");
+            }else if(i==7&&num==7){
+                System.out.println("East End");
+            }else if(i==8&&num==8){
+                System.out.println("RagiGudda");
+            }else if(i==9&&num==9){
+                System.out.println("Rayuru Matta");
+            }else if(i==10&&num==10){
+                System.out.println("BTM Bus Stop");
+            }
+        }
+        int s=3;
+        int num1=1;
+        while(num1<=10){
+          if(s==1&&num1==1){
+                System.out.println("Silk Board");
+            }else if(s==2&&num1==2){
+                System.out.println("Majestic");
+            }else if(s==3&&num1==3){
+                System.out.println("MICO Layout");
+            }else if(s==4&&num1==4){
+                System.out.println("BTM Layout");
+            }else if(s==5&&num1==5){
+                System.out.println("Kasturi Nagar");
+            }else if(s==6&&num1==6){
+                System.out.println("BTM WaterTank");
+            }else if(s==7&&num1==7){
+                System.out.println("East End");
+            }else if(s==8&&num1==8){
+                System.out.println("RagiGudda");
+            }else if(s==9&&num1==9){
+                System.out.println("Rayuru Matta");
+            }else if(s==10&&num1==10){
+                System.out.println("BTM Bus Stop");
+            }  
+            num1++;
+        }
+        int num2=1;
+        int k=2;
+        do{
+            if(k==1&&num2==1){
+                System.out.println("Silk Board");
+            }else if(k==2&&num2==2){
+                System.out.println("Majestic");
+            }else if(k==3&&num2==3){
+                System.out.println("MICO Layout");
+            }else if(k==4&&num2==4){
+                System.out.println("BTM Layout");
+            }else if(k==5&&num2==5){
+                System.out.println("Kasturi Nagar");
+            }else if(k==6&&num2==6){
+                System.out.println("BTM WaterTank");
+            }else if(k==7&&num2==7){
+                System.out.println("East End");
+            }else if(k==8&&num2==8){
+                System.out.println("RagiGudda");
+            }else if(k==9&&num2==9){
+                System.out.println("Rayuru Matta");
+            }else if(k==10&&num2==10){
+                System.out.println("BTM Bus Stop");
+            }
+            num2++;
+        }while(num2<=10);
+    }
+}
