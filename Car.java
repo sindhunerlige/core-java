20c4d41ed0485d1ef07bc2483a06bea871d3996c
sindhunerlige <sindhunerlige@gmail.com>
2025-12-19 09:42:05 +0530

    car brand

---
+++ Car.java
@@ -0,0 +1,16 @@
+ class Car{
+
+    static String name="Audi";
+    String color ="White";
+
+    public static void main(String[] args){
+        int mileage=70;
+        System.out.println(mileage);
+
+        Car c1 = new Car();
+        System.out.println(c1.name);
+
+        Car c2 = new Car();
+        System.out.println(c2.color);
+    }
+}
