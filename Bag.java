34e2f407dc76185c09de956a99519697f19bd38a
sindhunerlige <sindhunerlige@gmail.com>
2025-12-19 09:36:39 +0530

    Bag file

---
+++ Bag.java
@@ -0,0 +1,12 @@
+    class Bag {
+    static int cost=500;
+    String type="Handbag";
+    public static void main(String args[]){
+        Bag bg = new Bag();
+        System.out.println(bg.cost);
+
+        Bag bg1 = new Bag();
+        System.out.println(bg1.type);
+
+    }
+}
