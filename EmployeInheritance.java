32e9b7147c6ff76a7a24af28c8485aeb42fd6b99
sindhunerlige <sindhunerlige@gmail.com>
2026-01-03 18:33:21 +0530

    employ inheritance

---
+++ EmployeInheritance.java
@@ -0,0 +1,17 @@
+class EmployeInheritance extends Employes {
+String department;
+int Age;
+
+public void setDepartment(String department){
+this.department=department;
+}
+public String getDepartment(){
+return department;
+}
+public void setAge(int Age){
+this.Age=Age;
+}
+public int getAge(){
+return Age;
+}
+}
