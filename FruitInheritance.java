d0e015c9d46a30419ddc06e5750a52e274a41930
sindhunerlige <sindhunerlige@gmail.com>
2026-01-03 19:48:20 +0530

    hi

---
+++ FruitInheritance.java
@@ -0,0 +1,17 @@
+class FruitInheritance extends Fruit{
+String seasonfruit;
+String color;
+
+public void setSeasonFruit(String seasonfruit){
+this.seasonfruit=seasonfruit;
+}
+public String getSeasonFruit(){
+return seasonfruit;
+}
+public void setColor(String color){
+this.color=color;
+}
+public String getColor(){
+return color;
+}
+}
---
+++ FruitInheritanceRunner.java
@@ -0,0 +1,9 @@
+class FruitInheritanceRunner{
+public static void main(String[] args){
+FruitInheritance fruit =new FruitInheritance();
+fruit.setColor("Red");
+System.out.println(fruit.getColor());
+fruit.setSeasonFruit("winter");
+System.out.println(fruit.getSeasonFruit());
+}
+}
