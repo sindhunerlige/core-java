b1a1cb2141ab5b5dba756225f284e9d1c01f49d7
sindhunerlige <sindhunerlige@gmail.com>
2026-01-03 18:34:25 +0530

    employ inheritance runner

---
+++ EmployeeInheritanceRunner.java
@@ -0,0 +1,14 @@
+class EmployeeInheritanceRunner{
+public static void main(String[] args){
+EmployeInheritance employe = new EmployeInheritance();
+employe.setDepartment("cloud");
+System.out.println(employe.getDepartment());
+employe.setAge(24);
+System.out.println(employe.getAge());
+employe.setName("sindhu");
+System.out.println(employe.getName());
+employe.setSalary(45000);
+System.out.println(employe.getSalary());
+}
+}
+
