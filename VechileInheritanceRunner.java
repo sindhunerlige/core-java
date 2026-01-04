7e6b17ae76d84dd5ff1b54c5d107214ad84220af
sindhunerlige <sindhunerlige@gmail.com>
2026-01-05 00:29:42 +0530

    hello

---
+++ VechileInheritanceRunner.java
@@ -0,0 +1,8 @@
+    class VechileInheritanceRunner {
+    public static void main(String[] args){
+        VechileInheritance vechilceinheritance = new VechileInheritance();
+        vechilceinheritance.setSound("horn");
+        System.out.println(vechilceinheritance.getSound());
+        System.out.println(vechilceinheritance.getModel());
+    }
+}
