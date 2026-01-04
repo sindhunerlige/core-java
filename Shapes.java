634822b8896196bd52ca826c2ea4a77e28c93339
sindhunerlige <sindhunerlige@gmail.com>
2026-01-05 00:09:36 +0530

    shapes

---
+++ Shapes.java
@@ -0,0 +1,26 @@
+    class Shapes {
+    String square;
+    String triangle;
+
+   public Shapes(){
+
+   }
+   public Shapes(String square,String triangle){
+    this.square=square;
+    this.triangle=triangle;
+   }
+   public static void main(String args[]){
+   } 
+   public void setSquare(String square){
+    this.square=square;
+   }
+   public String getSquare(){
+    return square;
+   }
+   public void setTriangle(String triangle){
+    this.triangle=triangle;
+   }
+   public String getTriangle(){
+	   return triangle;
+   }
+}
