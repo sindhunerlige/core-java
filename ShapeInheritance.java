55c263931b7e3b12531537f49b4ff47374507047
sindhunerlige <sindhunerlige@gmail.com>
2026-01-05 00:10:51 +0530

    hello

---
+++ ShapeInheritance.java
@@ -0,0 +1,9 @@
+    class ShapeInheritance extends Shapes {
+    String cube;
+    public void setCube(String cube){
+        this.cube=cube;
+    }
+    public String getCube(){
+        return cube;
+    }
+}
