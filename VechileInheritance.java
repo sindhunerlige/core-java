779edf638a41ff5a2d97025a845e7f035cc2a463
sindhunerlige <sindhunerlige@gmail.com>
2026-01-05 00:28:47 +0530

    hi

---
+++ VechileInheritance.java
@@ -0,0 +1,17 @@
+class VechileInheritance extends Vehicles{
+    String sound;
+    String name;
+
+    public void setSound(String sound){
+        this.sound=sound;
+    }
+    public String getSound(){
+        return sound;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+}
