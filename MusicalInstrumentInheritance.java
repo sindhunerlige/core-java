b43894f3c893ecbf1331b26bd309ac95c7d0ec35
sindhunerlige <sindhunerlige@gmail.com>
2026-01-05 00:18:58 +0530

    musicalinheritance

---
+++ MusicalInstrumentInheritance.java
@@ -0,0 +1,10 @@
+class MusicalInstrumentInheritance extends MusicalInstrument {
+    String drum;
+
+    public void setDrum(String drum){
+        this.drum=drum;
+    }
+    public String drum(){
+        return drum;
+    }
+}
